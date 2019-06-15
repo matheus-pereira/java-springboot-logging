844586d79f5bca91167c86406a0ea2965735c8bc
Matheus Pereira <matheus.spereira@hotmail.com>
2019-06-15 10:19:52 -0300

    Add user model

---
+++ src/main/java/tech/pereira/logging/domain/User.java
@@ -0,0 +1,87 @@
+package tech.pereira.logging.domain;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;;
+
+@JsonInclude(Include.NON_NULL)
+public class User {
+
+	@Id
+	private String id;
+	
+	@NotNull
+	@NotBlank
+	private String name;
+	
+	@NotNull
+	@NotBlank
+	private String email;
+	
+	private String phone;
+	
+	@CreatedDate
+	@JsonIgnore
+	private Date createdAt;
+	
+	@LastModifiedDate
+	@JsonIgnore
+	private Date modifiedAt;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email.toLowerCase();
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getModifiedAt() {
+		return modifiedAt;
+	}
+
+	public void setModifiedAt(Date modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+}
